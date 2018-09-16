1285e733074d5bc5295b19d14c05b7d1ec14ff5b
AlexeyKarasev <karasev.alexey@gmail.com>
2018-09-16 09:47:33 +0300

    lints fix

--- app/server.go
+++ app/server.go
@@ -6,12 +6,13 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// StartServer starts main app http server
 func StartServer() {
 	r := mux.NewRouter()
-	r.HandleFunc("/", HomeHandler)
+	r.HandleFunc("/", homeHandler)
 	http.ListenAndServe(":8000", r)
 }
 
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello world"))
 }
